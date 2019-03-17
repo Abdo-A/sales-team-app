686c59a2f32f3c31a8a3a75c899738d093c81b74
Abdo <abdopoetry@gmail.com>
2019-03-17 23:55:47 +0200

    Allow sending a notification to superadmin when a new user has registered, for approval

--- backend/controllers/userController/registerUser.js
+++ backend/controllers/userController/registerUser.js
@@ -1,6 +1,8 @@
 const bcrypt = require('bcryptjs');
 const mongoose = require('mongoose');
 
+const sendNotifications = require('../../assets/utils/sendNotifications');
+
 // Translation files
 const userRelatedData = require('../../assets/data/translations/userRelatedData');
 
@@ -43,6 +45,36 @@ module.exports = (req, res) => {
           newUser
             .save()
             .then((savedUser) => {
+              // Send notification to superadmin to approve this user
+              User.find({ type: 'superadmin' }).then((superadmins) => {
+                const messages = [];
+
+                superadmins.forEach((superadmin) => {
+                  if (!superadmin.pushNotificationToken) {
+                    return;
+                  }
+
+                  let notificationBody = 'A new user has registered';
+                  if (Array.isArray(newUser.DCs) && newUser.DCs.length > 0) {
+                    notificationBody = `${notificationBody} from ${
+                      newUser.DCs[0]
+                    }`;
+                  }
+
+                  messages.push({
+                    to: superadmin.pushNotificationToken,
+                    sound: 'default',
+                    title: `${newUser.firstName} ${
+                      newUser.surname
+                    } has just resigtered`,
+                    body: notificationBody,
+                  });
+                });
+                if (messages.length > 0) {
+                  sendNotifications(messages);
+                }
+              });
+
               res.json(savedUser);
             })
             .catch((dbError) => {
